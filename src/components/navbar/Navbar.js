40f7fad99d0b89e6da4565906f162169c741ec2c
gunwoo.jung <doscmDev@gmail.com>
2018-11-18 17:42:59 +0900

    Add : tu.png & navbar
    네비게이션바에 동명대 이미지 삽입

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { AppBar } from 'material-ui';
-
-const Navbar = () => <AppBar title="Image Search"/>
+import { AppBar, Menu, MenuItem } from 'material-ui';
+import tu from './tu.png'
+const Navbar = () => {
+    const style = {
+        height:"30%",
+        borderRadius: "50px"
+    }
+    return (
+        <AppBar title="Image Search">
+            <Menu>
+                <img style={style} src={tu} alt=""/>
+            </Menu>
+        </AppBar>
+    )
+}
 export default Navbar;

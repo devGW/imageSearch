f61aa05caa563e5db8d3587e9146652db377b558
gunwoo.jung <doscmDev@gmail.com>
2018-11-18 17:43:30 +0900

    Modifiy : 함수명 변경
    Handle 로 통일

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -10,59 +10,61 @@ class Search extends Component {
         search: "",
         amount: 15,
         apiUrl: "https://pixabay.com/api",
-        apiKey: '10726265-4c30be9345939ece9f7e25ad1',
+        apiKey: "10726265-4c30be9345939ece9f7e25ad1",
         images: []
-
-    }
-
-    onTextChange = e => {
+      };
+    
+      handleChange = e => {
         const val = e.target.value;
         this.setState({ [e.target.name]: val }, () => {
-            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}
-            &q=${this.state.search}&image_type=photo
-            &per_page=${this.state.amount}&safesearch=true`)
-                .then(res => this.setState({ images: res.data.hits }))
-                .catch(err => console.log(err));
-
-        })
-    }
-    onAmountChange = (e, index, value) => {
-        this.setState({
-            amount : value
-        })
-    }
-    render() {
-        console.log(this.state.images)
+          if (val === "") {
+            this.setState({ images: [] });
+          } else {
+            axios
+              .get(
+                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+                  this.state.search
+                }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+              )
+              .then(res => this.setState({ images: res.data.hits }))
+              .catch(err => console.log(err));
+          }
+        });
+      };
+    
+      handleAmount = (e, index, value) => this.setState({ amount: value });
+    
+      render() {
+        console.log(this.state);
         return (
-            <div>
-                <TextField
-
-                    name="search"
-                    value={this.state.searchText}
-                    onChange={this.onTextChange}
-                    placeholder="Search Image"
-                    fullWidth={true}
-                />
-                <br />
-                <SelectField
-                    name="amount"
-                    floatingLabelText="amount"
-                    value={this.state.amount}
-                    onChange={this.onAmountChange}
-
-                >
-                    <MenuItem value={5} primaryText="5" />
-                    <MenuItem value={10} primaryText="10" />
-                    <MenuItem value={15} primaryText="15" />
-                    <MenuItem value={30} primaryText="30" />
-                    <MenuItem value={50} primaryText="50" />
-                </SelectField>
-                <br/>
-                {this.state.images.length > 0 ? 
-                    (<ImageResult images={this.state.images}/>) : null}
-            </div>
+          <div>
+            <TextField
+              name="search"
+              value={this.state.search}
+              onChange={this.handleChange}
+              floatingLabelText="이미지 검색"
+              fullWidth={true}
+            />
+            <br />
+            <SelectField
+              name="amount"
+              floatingLabelText="개수"
+              value={this.state.amount}
+              onChange={this.handleAmount}
+            >
+              <MenuItem value={5} primaryText="5" />
+              <MenuItem value={10} primaryText="10" />
+              <MenuItem value={15} primaryText="15" />
+              <MenuItem value={30} primaryText="30" />
+              <MenuItem value={50} primaryText="50" />
+            </SelectField>
+            <br />
+            {this.state.images.length > 0 ? (
+              <ImageResult images={this.state.images} />
+            ) : null}
+          </div>
         );
-    }
+      }
 }
 
 export default Search;

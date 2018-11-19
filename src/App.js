b967fc82dd819a696904badb608311bee3f9678b
gunwoo.jung <doscmDev@gmail.com>
2018-11-19 23:39:24 +0900

    Feat : add state
    이미가 없을때 하단에 footer를 고정하기 위한  section 을 control 하기 위한 state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Search from './components/search/Search';
+import FooterPage from './components/footer/FooterPage';
+import Section from './components/section/Section';
 import { MuiThemeProvider } from 'material-ui/styles';
+import Search from './components/search/Search';
+
+
 
 class App extends Component {
+  state = {
+    space : true
+  }
+  handleState = () => {
+    this.setState({
+      space : !this.state.space
+    })
+  }
   render() {
+    console.log(this.state.space);
     return (
+      
       <MuiThemeProvider>
         <div>
           <Navbar/>
-          <Search/>
+          <Search handleSpace={this.handleState}/>
+          {
+            this.state.space && <Section/>
+          }
+          <FooterPage/>
         </div>
       </MuiThemeProvider>
     );

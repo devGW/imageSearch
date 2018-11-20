6219d63636edb2982bfcd609fa2b317990df0d68
gunwoo.jung <doscmDev@gmail.com>
2018-11-20 13:05:23 +0900

    Feat : Footer
    Section : 이미지 뜨기전에 Section 으로 빈공간 채움
    Footer : Footer 양식

---
+++ src/components/section/Section.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Section extends Component {
+    render() {
+        const style = {
+            height : "232px"
+        }
+        return (
+            <div style={style}>
+            </div>
+        );
+    }
+}
+
+export default Section;
